0e098ad63d3e9a96a0a8a949d419fe3957874da4
Benjamin Osment <benjamin.j.osment@gmail.com>
2012-11-25 11:51:22 -0500

    added calculating probabilites and classifying

--- code/classification/NBClassifier.java
+++ code/classification/NBClassifier.java
@@ -3,42 +3,49 @@ package classification;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class NBClassifier {
 	int n; // total number of training instances
 	int num_pos; // positive training instances
 	int num_neg; // negative training instances
 	// array of hash tables for the positive frequency
-	ArrayList<HashMap<String, Integer>> pos_freq; 
+	ArrayList<Map<String, Integer>> pos_freq; 
 	// array of hash tables for the negative frequency
-	ArrayList<HashMap<String, Integer>> neg_freq;
+	ArrayList<Map<String, Integer>> neg_freq;
 	// array of sets, each set corresponds to one feature
-	ArrayList<HashSet<String>> feature_set;
+	ArrayList<Set<String>> feature_set;
 	// array of hash tables for probability
-	ArrayList<HashMap<String, Double>> p;
-	// TODO -- just use 1? negative is 1 - p?
+	ArrayList<Map<String, Double>> p_pos;
+	ArrayList<Map<String, Double>> p_neg;
 	boolean initialized = false;
+	double p_pos_class; // probability of a positive class 
+	double p_neg_class; // probability of a negative class
 	
 	public NBClassifier() {
 		// initialize the frequency counts and data structures
 		n = 0; 
 		num_pos = 0; 
 		num_neg = 0;
-		pos_freq = new ArrayList<HashMap<String, Integer>>();
-		neg_freq = new ArrayList<HashMap<String, Integer>>();
-		feature_set = new ArrayList<HashSet<String>>();
-		p = new ArrayList<HashMap<String, Double>>();
+		pos_freq = new ArrayList<Map<String, Integer>>();
+		neg_freq = new ArrayList<Map<String, Integer>>();
+		feature_set = new ArrayList<Set<String>>();
+		p_pos = new ArrayList<Map<String, Double>>();
+		p_neg = new ArrayList<Map<String, Double>>();
 	}
 	
 	public void train(TrainingExample train_ex) {
-		ArrayList<HashMap<String, Integer>> freq; 
+		ArrayList<Map<String, Integer>> freq; 
 		if (initialized == false) {
 			// create HashMaps/Sets for all of our data structures
 			for (int i = 0; i < train_ex.getFeatures().length; i++) {
 				pos_freq.add(new HashMap<String, Integer>());
 				neg_freq.add(new HashMap<String, Integer>());
 				feature_set.add(new HashSet<String>());
-				p.add(new HashMap<String, Double>());
+				p_pos.add(new HashMap<String, Double>());
+				p_neg.add(new HashMap<String, Double>());				
 			}
 			initialized = true;
 		}
@@ -55,7 +62,7 @@ public class NBClassifier {
 		String[] features = train_ex.getFeatures();			
 		for (int i = 0; i < features.length; i++){
 			if (freq.get(i).containsKey(features[i])) {
-				freq.get(i).put(features[i], freq.get(i).get(features[i])+ 1);
+				freq.get(i).put(features[i], freq.get(i).get(features[i]) + 1);
 			} else {
 				// if we haven't see it before, initialize first occurrence
 				freq.get(i).put(features[i], 1);
@@ -67,10 +74,67 @@ public class NBClassifier {
 		n++; 
 	}
 
+	public void calculate_probabilities() {
+		// TODO: is log correct here
+		p_pos_class = Math.log((double)num_pos / n);
+		p_neg_class = Math.log((double)num_neg / n);
+		
+		// just calculate positive frequent, negative is the complement
+		for (int i = 0; i < pos_freq.size(); i++) {
+			// for each feature, update probability
+			int denominator = num_pos + feature_set.get(i).size();
+			Iterator<String> iter = feature_set.get(i).iterator();
+			while (iter.hasNext()){
+				int numerator = 1; // Laplacian smoothing
+				String feature = iter.next();
+				if (pos_freq.get(i).containsKey(feature)) {
+					numerator += pos_freq.get(i).get(feature);
+				}
+				
+				// update probability, use logarithms to avoid underflow
+				p_pos.get(i).put(feature, Math.log((double)numerator/denominator));
+			}
+		}
+		
+		for (int i = 0; i < neg_freq.size(); i++) {
+			// for each feature, update probability
+			int denominator = num_neg + feature_set.get(i).size();
+			Iterator<String> iter = feature_set.get(i).iterator();
+			while (iter.hasNext()){
+				int numerator = 1; // Laplacian smoothing
+				String feature = iter.next();
+				if (neg_freq.get(i).containsKey(feature)) {
+					numerator += neg_freq.get(i).get(feature);
+				}
+				
+				// update probability, use logarithms to avoid underflow
+				p_neg.get(i).put(feature, Math.log((double)numerator/denominator));
+			}
+		}
+	}
+	
+	
 	@Override
 	public String toString() {
 		return "NBClassifier [n=" + n + ", num_pos=" + num_pos + ", num_neg="
 				+ num_neg + ", pos_freq=" + pos_freq + ", neg_freq="
-				+ neg_freq + ", feature_set=" + feature_set + "]";
+				+ neg_freq + ", feature_set=" + feature_set
+				+ ", p_pos_class=" + p_pos_class + ", p_neg_class=" + 
+				p_neg_class + ", p_pos=" + p_pos + ", p_neg=" + p_neg + "]";
+	}
+
+	public String classify(String[] features) {
+		double pos_prob = p_pos_class;
+		double neg_prob = p_neg_class;
+		for (int i = 0; i < features.length; i++){
+			pos_prob += p_pos.get(i).get(features[i]);
+			neg_prob += p_neg.get(i).get(features[i]);
+		}
+		
+		if (pos_prob > neg_prob) {
+			return "+1";
+		} else {
+			return "-1";
+		}
 	}
 }
--- code/classification/NaiveBayes.java
+++ code/classification/NaiveBayes.java
@@ -5,6 +5,11 @@ import java.util.Arrays;
 
 public class NaiveBayes {
 	public static void main (String [] args) {
+		int tp = 0;
+		int tn = 0;
+		int fp = 0;
+		int fn = 0;
+
 		if (args.length < 2) {
 			System.out.println("Usage: java NaiveBayes, training_file testing_file");
 			System.exit(0);
@@ -16,7 +21,8 @@ public class NaiveBayes {
 			NBClassifier nbclass = new NBClassifier();
 			
 			// TODO: move the training section into a function/method?
-			// TODO: add logging? 
+			// TODO: add logging?
+			
 			// read in the training file, print label, number of attributes, total examples
 			System.out.format("Reading training file: %s\n", args[0]);
 			try {
@@ -41,7 +47,43 @@ public class NaiveBayes {
 				ex.printStackTrace();
 			}
 			
-			System.out.println(nbclass);
+			// calculate the probabilities for each class
+			nbclass.calculate_probabilities();
+			
+			// classify each test instance
+			System.out.format("Reading test file: %s\n", args[1]);
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(args[1]));
+				String line = null;
+				while ((line = reader.readLine()) != null) {
+					String[] splitLine = line.split("\t", -1);
+					String label = splitLine[0];
+					String predicted_label;
+					// TODO need a better way of doing this
+					String[] features = Arrays.copyOfRange(splitLine, 1, splitLine.length); 
+										predicted_label = nbclass.classify(features);
+					if (label.equals("+1")) {
+						if (predicted_label.equals("+1")) {
+							tp += 1;
+						} else {
+							fn += 1;
+						}
+					} else {
+						if (predicted_label.equals("-1")) {
+							tn += 1;
+						} else {
+							fp += 1;
+						}
+					}
+				}
+				reader.close();
+				
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+			
+			//System.out.println(nbclass);
+			System.out.format("TP: %d TN %d FP %d FN %d\n", tp, tn, fp, fn);
 		}
 	}
 }
